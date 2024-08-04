41c0db5bd148b5ac9c4c27135ca11890ef901810
Keyla Soriano <44504724+ksoriano55@users.noreply.github.com>
2024-08-03 19:10:00 -0600

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   //base: "/nowa-ts/preview/",  // Use base path for while deploying the project the SSR.
   plugins: [react()],
+  base: "/CinepolisWebApp",
   define: {
     'process.env': {},
   },
